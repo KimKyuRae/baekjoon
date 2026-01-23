a9940c4255296d20f3d660abb66a53c436678758
KYURAE KIM <mootomgame@gmail.com>
2026-01-23 20:31:33 +0900

    1008

---
+++ 1008.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let nums: Vec<f64> = input.split_whitespace()
+        .map(|s| s.parse::<f64>().unwrap())
+        .collect();
+
+    println!("{}", nums[0] / nums[1]);
+}
